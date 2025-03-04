0560d71e37eb2ba797ce792232393cfbbfb09e9c
a7889654 <a7889654@gmail.com>
2025-03-04 20:34:22 +0800

    Add nvim-treesitter-context to show function or class name at the top

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -24,6 +24,7 @@ return require('packer').startup(function(use)
       {run = ':TSUpdate'}
   }
   use {'nvim-treesitter/playground'}
+  use {'nvim-treesitter/nvim-treesitter-context'}
   use {'theprimeagen/harpoon'}
   use {'mbbill/undotree'}
   use {'tpope/vim-fugitive'}
